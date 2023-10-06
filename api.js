8c3789ca5ea829e8eb6331022998ffd273c39f5d
ThomasRavetto <travetto18@gmail.com>
2023-10-06 08:46:59 +0200

    adding api

---
+++ api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const{ fibonacci } = require('./fibonacci')
+
+const PORT = 3500;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hi")
+})
+
+
+// Return fibonacci sequence from 0 to index, if no query is passed index defaults to 10
+app.get('/fibonacci', (req, res) => {
+    let index = 10;
+    if (req.query && req.query.index) {
+        index = req.query.index;
+    }
+    const fibonacci_array = fibonacci(index);
+    res.json(fibonacci_array);
+})
+
+app.listen(PORT, () => {
+    console.log("Server working on port: ", PORT);
+})
--- fibonacci.js
+++ fibonacci.js
@@ -17,4 +17,4 @@ function fibonacci(index) {
     }
 }
 
-
+module.exports = { fibonacci }
